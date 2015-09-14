dc69c66d4b97941d6b4165a16bd8adfa8d964463
Sabab <Sabab@DESKTOP-J9S9A7V>
2015-09-14 13:13:21 +0600

    Commit :Sept 14

---
+++ application/views/vat.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VAT</title>
+    
+    <!-- Styles -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,700,800" rel="stylesheet" type="text/css"><!-- Google web fonts -->
+    <link href="<?php echo base_url(); ?>font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"><!-- font-awesome -->
+    <link href="<?php echo base_url(); ?>js/dropdown-menu/dropdown-menu.css" rel="stylesheet" type="text/css"><!-- dropdown-menu -->
+    <link href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"><!-- Bootstrap -->
+    <link href="<?php echo base_url(); ?>js/fancybox/jquery.fancybox.css" rel="stylesheet" type="text/css"><!-- Fancybox -->
+    <link href="<?php echo base_url(); ?>js/audioplayer/audioplayer.css" rel="stylesheet" type="text/css"><!-- Audioplayer -->
+    <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet" type="text/css"><!-- theme styles -->
+    <link href="<?php echo base_url(); ?>css/logo.css" rel="stylesheet" type="text/css">
+
+  </head>
+  
+  <body role="document" class="page">
+  
+    <!-- device test, don't remove. javascript needed! -->
+    <span class="visible-xs"></span><span class="visible-sm"></span><span class="visible-md"></span><span class="visible-lg"></span>
+    <!-- device test end -->
+    
+    <div id="k-head" class="container"><!-- container + head wrapper -->
+    
+        <div class="row"><!-- row -->
+        
+            <nav class="k-functional-navig"><!-- functional navig -->
+        
+                <ul class="list-inline pull-right">
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Calendar</a></li>
+                    <li><a href="#">Directions</a></li>
+                </ul>
+        
+            </nav><!-- functional navig end -->
+        
+            <div class="col-lg-12">
+        
+                <div id="k-site-logo" class="pull-left"><!-- site logo -->
+                
+                    <h1 class="k-logo">
+                        <a href="index.html" title="Home Page">
+                            <img src="<?php echo base_url(); ?>img/site-logo.png" alt="Site Logo" class="img-responsive" />
+                        </a>
+                    </h1>
+                    
+                    <a id="mobile-nav-switch" href="#drop-down-left"><span class="alter-menu-icon"></span></a><!-- alternative menu button -->
+            
+                </div><!-- site logo end -->
+
+                <nav id="k-menu" class="k-main-navig"><!-- main navig -->
+        
+                    <ul id="drop-down-left" class="k-dropdown-menu">
+                        <li>
+                            <a href="news.html" title="Our School News">News</a>
+                        </li>
+                        <li>
+                            <a href="events.html" title="Upcoming Events">Events</a>
+                        </li>
+                        <li>
+                            <a href="courses.html" title="Available Courses">Courses</a>
+                        </li>
+                        <li>
+                            <a href="#" class="Pages Collection" title="More Templates">Inner Pages</a>
+                            <ul class="sub-menu">
+                                <li><a href="news-single.html">News Single Page</a></li>
+                                <li><a href="events-single.html">Events Single Page</a></li>
+                                <li><a href="courses-single.html">Course Single Page</a></li>
+                                <li><a href="gallery-single.html">Gallery Single Page</a></li>
+                                <li><a href="news-stacked.html">News Stacked Page</a></li>
+                                <li><a href="search-results.html">Search Results Page</a></li>
+                                <li>
+                                    <a href="#">Menu Test</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Second Level 01</a></li>
+                                        <li>
+                                            <a href="#">Second Level 02</a>
+                                            <ul class="sub-menu">
+                                                <li><a href="#">Third Level 01</a></li>
+                                                <li><a href="#">Third Level 02</a></li>
+                                                <li><a href="#">Third Level 03</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Second Level 03</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="current-menu-item">
+                            <a href="about-us.html" title="How things work">About Us</a>
+                            <ul class="sub-menu">
+                                <li><a href="full-width.html">Full Width Page</a></li>
+                                <li><a href="sidebar-left.html">Sidebar on Left</a></li>
+                                <li><a href="formatting.html">Formatting</a></li>
+                                <li><a href="school-leadership.html">School Leadership</a></li>
+                                <li><a href="gallery.html">Gallery</a></li>
+                                <li><a href="404.html">404 Error</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="contact-us.html" title="School Contacts">Contact Us</a>
+                        </li>
+                    </ul>
+        
+                </nav><!-- main navig end -->
+            
+            </div>
+            
+        </div><!-- row end -->
+    
+    </div><!-- container + head wrapper end -->
+    
+    <div id="k-body"><!-- content wrapper -->
+    
+        <div class="container"><!-- container -->
+        
+            <div class="row"><!-- row -->
+            
+                <div id="k-top-search" class="col-lg-12 clearfix"><!-- top search -->
+                
+                    <form action="#" id="top-searchform" method="get" role="search">
+                        <div class="input-group">
+                            <input type="text" name="s" id="sitesearch" class="form-control" autocomplete="off" placeholder="Type in keyword(s) then hit Enter on keyboard" />
+                        </div>
+                    </form>
+                    
+                    <div id="bt-toggle-search" class="search-icon text-center"><i class="s-open fa fa-search"></i><i class="s-close fa fa-times"></i></div><!-- toggle search button -->
+                
+                </div><!-- top search end -->
+            
+                <div class="k-breadcrumbs col-lg-12 clearfix"><!-- breadcrumbs -->
+                
+                    <ol class="breadcrumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active">About Us</li>
+                    </ol>
+                    
+                </div><!-- breadcrumbs end -->               
+                
+            </div><!-- row end -->
+            
+            <div class="row no-gutter"><!-- row -->
+                
+                <div class="col-lg-8 col-md-8"><!-- doc body wrapper -->
+                    
+                    <div class="col-padded"><!-- inner custom column -->
+                    
+                        <div class="row gutter"><!-- row -->
+                        
+                            <div class="col-lg-12 col-md-12">
+                    
+                                
+                                <h1 class="page-title">VAT</h1>
+                                
+                                <div class="news-body">
+                                
+                                    
+
+                                    <p class="call-out">
+                                        VAT legislation is an area we know like the back of our hand. We are able to offer accurate, timely assistance in a number of areas. These include:
+
+                                        <ul class="l1">
+                                            <li>Registration queries</li>
+                                            <li>Preparation of VAT retrurns</li>
+                                            <li>Filing and adjusting VAT returns</li>
+                                            <li>IRD audit assitance</li>
+                                            <li>Specialist events involving VAT</li>
+                                            <li>Claimiming Rebate</li>
+                                            
+                                        </ul>
+                                    </p>
+
+                                    <h4 style="margin-top:15px; margin-bottom:15px;">Registration:</h4>
+                                    <p class="call-out">
+                                        Every producer of taxable goods and services are required to registered under the VAT Act-1991 if his annul turnover exceed Tk 40 Lac. Separate registration for every production center shall be required. But in case of sales outlet Central Registration may be applied for but warehouse should have to be maintained centrally.
+                                    </p>
+
+                                    <h4 style="margin-top:15px; margin-bottom:15px;">Vat Rate:</h4>
+                                    <p class="call-out">
+                                        Generally 15% is chargeable on value addition but incase of services there may some deemed value addition.
+                                    </p>
+
+                                    <h5 style="margin-top:15px; margin-bottom:15px;">Submission on Vat Return:</h5>
+                                    <p class="call-out">
+                                        Two copies of return (Mushak-19) shall be submitted within 10 days from the tax period to the proper officer( 20th day for insurance company) by attaching the following documents:
+                                    </p>
+
+                                    <ol>
+                                        <li>Copies of Current Account</li>
+                                        <li>The bill of entry or Mushak-11 relating to purchase</li>
+                                        <li>Original &amp; Second copy of treasury challan in case of service</li>
+                                        <li>Any other documents required by Commissioner</li>
+                                    </ol>
+
+                                    <p style="margin-top:15px;">
+                                        <a href="contact_us.html">Contact us</a> today for an appointment and expert advice to aid your situation.
+                                    </p>
+                                    
+                                </div>
+                            
+                            </div>
+                        
+                        </div><!-- row end -->
+                                
+                    
+                    </div><!-- inner custom column end -->
+                    
+                </div><!-- doc body wrapper end -->
+                
+                <div id="k-sidebar" class="col-lg-4 col-md-4"><!-- sidebar wrapper -->
+                    
+                    <div class="col-padded col-shaded"><!-- inner custom column -->
+                    
+                        <ul class="list-unstyled clear-margins"><!-- widgets -->
+                        
+                            <li class="widget-container widget_nav_menu"><!-- widget -->
+                    
+                                <h1 class="title-widget">Select</h1>
+                                
+                                <ul>
+                                    <li><a href="#" title="menu item">News Archive</a></li>
+                                    <li><a href="#" title="menu item">Tradition of School Events</a></li>
+                                    <li><a href="#" title="menu item">Report Asocial Behaviour</a></li>
+                                    <li><a href="#" title="menu item">Trends and Tips</a></li>
+                                    <li><a href="#" title="menu item">Events Poll</a></li>
+                                </ul>
+                    
+                            </li>
+                            
+                            
+                    
+                    </div><!-- inner custom column end -->
+                    
+                </div><!-- sidebar wrapper end -->
+            
+            </div><!-- row end -->
+        
+        </div><!-- container end -->
+    
+    </div><!-- content wrapper end -->
+    
+     <?php include 'include/footer.php' ?>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>jQuery/jquery-2.1.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>jQuery/jquery-migrate-1.2.1.min.js"></script>
+    
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Drop-down -->
+    <script src="<?php echo base_url(); ?>js/dropdown-menu/dropdown-menu.js"></script>
+    
+    <!-- Fancybox -->
+    <script src="<?php echo base_url(); ?>js/fancybox/jquery.fancybox.pack.js"></script>
+    <script src="<?php echo base_url(); ?>js/fancybox/jquery.fancybox-media.js"></script><!-- Fancybox media -->
+    
+    <!-- Responsive videos -->
+    <script src="<?php echo base_url(); ?>js/jquery.fitvids.js"></script>
+    
+    <!-- Audio player -->
+    <script src="<?php echo base_url(); ?>js/audioplayer/audioplayer.min.js"></script>
+    
+    <!-- Pie charts -->
+    <script src="<?php echo base_url(); ?>js/jquery.easy-pie-chart.js"></script>
+    
+    <!-- Google Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
+    
+    <!-- Theme -->
+    <script src="<?php echo base_url(); ?>js/theme.js"></script>
+    
+  </body>
+</html>
