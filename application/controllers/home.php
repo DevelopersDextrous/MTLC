39a84d3cbe2ca3afd041b09dfe2586830935aeb5
shibz-islam <shibz.islam@gmail.com>
2015-09-16 18:18:35 +0600

    shihab 6.18 pm

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index(){
-		$this->load->view('index');
+		$this->load->model('article_model');
+		$data['article_count'] = $this->article_model->get_total_articles();
+		$this->load->model('director_model');
+		$data['director_count'] = $this->director_model->get_total_directors();
+
+		$this->load->view('index',$data);
 	}
 }
