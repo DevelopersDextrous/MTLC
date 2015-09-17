a5aed187482db3c90879e10bf8d1c0cba20f81a9
Sabab <Sabab@LaryPC01>
2015-09-17 18:38:59 +0600

    Sabab: 6.38pm

--- application/views/index.php
+++ application/views/index.php
@@ -49,33 +49,11 @@
                     <ul id="drop-down-left" class="k-dropdown-menu">
 
                         <li id="menu_1">
-                            <a href="#" class="Pages Collection" title="Our Provided Services">Services</a>
+                            <a href="#" class="Pages Collection" title="<b>Our Provided Services</b>">Services</a>
                             <ul class="sub-menu">
-                                <li><a href="news-single.html">Acquisitions &amp; Mergers</a></li>
-                                <li><a href="events-single.html">Internal Audit</a></li>
-                                <li><a href="courses-single.html">Benchmarking</a></li>
-                                <li><a href="gallery-single.html">Bookkeeping/Accounting</a></li>
-                                <li><a href="news-stacked.html">Budgeting</a></li>
-                                <li><a href="search-results.html">Cash flow Forecasting</a></li>
-                                <li><a href="search-results.html">Charitable Trusts</a></li>
-                                <li><a href="search-results.html">Estate Planning</a></li>
-                                <li><a href="#">Farm Accounting</a></li>
-                                <li><a href="#">Financing</a></li>
-                                <li><a href="#">Insolvency</a></li>
-                                <li><a href="#">Management Consulting</a></li>
-                                <li><a href="#">Office Duties</a></li>
-                                <li><a href="#">Payroll</a></li>
-                                <li><a href="#">Portfolio Management</a></li>
-                                <li><a href="#">Rental Property</a></li>
-                                <li><a href="#">Software Solutions</a></li>
-                                <li><a href="#">Start-up</a></li>
-                                <li><a href="#">Strategic Planning</a></li>
-                                <li><a href="#">Succession Planning</a></li>
-                                <li><a href="#">Taxation</a></li>
-                                <li><a href="#">Trusts</a></li>
-                                <li><a href="#">Valuations</a></li>
-                                <li><a href="#">Accounting Software</a></li>
-                                <li><a href="#">Business for Sale</a></li>
+                                
+                                <li><a href="#">Internal Auditing</a></li>
+                                <li><a href="#">Company Secretarial Matters</a></li>
                                 <li><a href="#">Tax</a></li>
                                 <li><a href="#">VAT</a></li>
                                 <li><a href="#">IRC</a></li>
@@ -90,8 +68,9 @@
                                 <li><a href="#">Utility Facilitation</a></li>
                                 <li><a href="#">Land Records &amp; Registration</a></li>
                                 <li><a href="#">Trade Mark</a></li>
-                                <li><a href="#">Patent/Design</a></li>
+                                <li><a href="#">Patent, Design</a></li>
                                 <li><a href="#">Copyright</a></li>
+                                <li><a href="#">CHambers Membership</a></li>
                                 <li><a href="#">Registration of Company</a></li>
                                 <li><a href="#">Partnership</a></li>
                                 <li><a href="#">Society/NGO/Cooperatives, Arbitration, Project Profile etc.</a></li>
@@ -100,13 +79,31 @@
                         </li>
 
                         <li id="menu_2">
-                            <a href="#" class="Pages Collection" title="Our Consultancy Services">Business Advisory</a>
+                            <a href="#" class="Pages Collection" title="<b>Our Consultancy Services</b>">Business Advisory</a>
                             <ul class="sub-menu">
-                                
+                                <li><a href="<?php echo base_url(); ?>index.php/service/acquisation_merger">Acquisitions &amp; Mergers</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/bookkeeping_accounting">Bookkeeping/Accounting</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/budgeting">Budgeting</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/charitable_trusts">Charitable Trusts</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/company_secretary">Company Secretary</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/estate_planning">Estate Planning</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/farm_accounting">Farm Accounting</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/insolvency">Insolvency</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/management_consulting">Management Consulting</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/office_duties">Office Duties</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/payroll_services">Payroll</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/portfolio_management">Portfolio Management</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/rental_property">Rental Property</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/software_solutions">Software Solutions</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/strategic_planning">Strategic Planning</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/succession_planning">Succession Planning</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/taxation">Taxation</a></li>
+                                <li><a href="<?php echo base_url(); ?>index.php/service/trusts">Trusts</a></li>
+                                <li><a href="#">Business for Sale</a></li>
                             </ul>
                         </li>
                         <li id="menu_3">
-                            <a href="about-us.html" title="How things work">About Us</a>
+                            <a href="about-us.html" title="<b>How Things Work</b>">About Us</a>
                             <ul class="sub-menu">
                                 <li><a href="#">Company Profile</a></li>
                                 <li>
@@ -121,7 +118,7 @@
                             </ul>
                         </li>
                         <li id="menu_4">
-                            <a href="contact-us.html" title="Office Contacts">Contact Us</a>
+                            <a href="contact-us.html" title="<b>Office Contacts</b>">Contact Us</a>
                         </li>
                     </ul>
         
